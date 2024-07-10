bf4d1d443a1f4e44968df0c0f5b165fda3829651
Samarjeet Singh <98832960+itsyoursamar@users.noreply.github.com>
2024-07-10 23:15:49 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -263,17 +263,7 @@ router.get('/failure',(req,res)=>{
     res.send("failed");
   })
 
-  router.all("*",(req,res,next)=>{
-    next(new ExpressError(404,"Page Not Found!"));
-    // req.flash("error","Page Not Found");
- 
-})
 
-router.use((err,req,res,next)=>{
-    let {statusCode=500,message="someting went wrong!"}=err;
-    res.status(statusCode).render("listings/Error.ejs",{message});
-    // res.status(statusCode).send(message);
-});
 
 module.exports=router;
 
